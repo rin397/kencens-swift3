3315e9c1a24997c45a2275fadd6eef614ca049fc
Wilson Hsieh <wilson.hsieh@bitsxbites.com>
2017-08-07 10:52:45 +0800

    update wilson file

---
+++ Wilson Hsieh/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let test = "test"
+
+var t = "hi"
+
+var dic = ["a":1,"b":2]
+
+print(dic["a"]!)
+
+var age=[String:Int]()
+age["w"] = 1
+print(age)
+
+var guess = arc4random()
+
+if(guess == 1){
+    print("1")
+}
